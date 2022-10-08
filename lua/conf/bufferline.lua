7020226ae2f7a0cef44bca3f778e52a340692a9c
yyooo <yyooo1216@gmail.com>
2022-10-08 22:30:05 +0800

    dress neovim

---
+++ lua/conf/bufferline.lua
@@ -0,0 +1,11 @@
+local status_ok, bufferline = pcall(require, "bufferline")
+if not status_ok then
+	return
+end
+
+local M = {}
+function M.setup()
+  bufferline.setup {
+  }
+end
+return M
---
+++ lua/plugins.lua
@@ -0,0 +1,83 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+-- packer.nvim configuration
+local conf = {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+require('packer').init(conf)
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  -- Beter icon
+  use {
+    "kyazdani42/nvim-web-devicons",
+    config = function()
+      require('nvim-web-devicons').setup { default = true }
+    end
+  }
+
+  -- Color
+  use {
+    "olimorris/onedarkpro.nvim",
+    config = function()
+      require('onedarkpro').setup({
+        options = {
+          transparency = true
+        }
+      })
+      vim.cmd("colorscheme onedarkpro")
+    end
+  }
+
+  -- Lualine
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = {
+      'kyazdani42/nvim-web-devicons',
+      opt = true
+    },
+    config = function()
+      require('conf.lualine').setup()
+    end
+  }
+
+  -- Bufferline
+  use {
+    'akinsho/bufferline.nvim',
+    tag = "v2.*",
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function()
+      require('conf.bufferline').setup()
+    end
+  }
+
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
