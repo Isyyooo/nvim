fccbebaca92fe2dfbeb524289fb71942f83036ca
yyooo <yyooo1216@gmail.com>
2023-07-23 12:55:44 +0800

    c debugger

--- init.lua
+++ init.lua
@@ -331,6 +331,38 @@ dap.listeners.before.event_exited["dapui_config"] = function()
   dapui.close()
 end
 
+dap.adapters.cppdbg = {
+  id = 'cppdbg',
+  type = 'executable',
+  command = '/home/yyooo/.local/share/nvim/mason/bin/OpenDebugAD7',
+}
+
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "cppdbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopAtEntry = true,
+  },
+  {
+    name = 'Attach to gdbserver :1234',
+    type = 'cppdbg',
+    request = 'launch',
+    MIMode = 'gdb',
+    miDebuggerServerAddress = 'localhost:1234',
+    miDebuggerPath = '/usr/bin/gdb',
+    cwd = '${workspaceFolder}',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+  },
+}
+dap.configurations.c = dap.configurations.cpp
+
 dap.adapters.python = function(cb, config)
 	if config.request == 'attach' then
 		---@diagnostic disable-next-line: undefined-field
@@ -369,3 +401,18 @@ dap.configurations.python = {
 		end
 	}
 }
+
+vim.keymap.set("n", "<Leader>de", function ()
+	dap.toggle_breakpoint()
+end)
+vim.keymap.set("n", "<Leader>dr", function ()
+	dap.continue()
+end)
+
+
+
+
+
+
+
+
